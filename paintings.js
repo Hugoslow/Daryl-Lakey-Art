8c53a498fc4c8fc886e157ba016b94a9567a23d6
Hugoslow <chris@chrislakey.com>
2026-09-14 00:28:23 +0100

    TBC

--- paintings.js
+++ paintings.js
@@ -60,7 +60,7 @@ const paintings = [
     },
     {
         title: "Breaking Through",
-        price: TBC,
+        price: tbc,
         sizeUnframed: 'TBC',
         sizeFramed: 'TBC',
         medium: "TBC",
@@ -70,7 +70,7 @@ const paintings = [
     },
     {
         title: "Wonder",
-        price: TBC,
+        price: tbc,
         sizeUnframed: 'TBC',
         sizeFramed: 'TBC',
         medium: "TBC",
