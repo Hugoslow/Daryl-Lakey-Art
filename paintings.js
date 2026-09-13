6896afb476f72bec633049542838472f184ef9ab
Hugoslow <chris@chrislakey.com>
2026-09-14 00:25:14 +0100

    Prices

--- paintings.js
+++ paintings.js
@@ -60,7 +60,7 @@ const paintings = [
     },
     {
         title: "Breaking Through",
-        price: null,
+        price: TBC,
         sizeUnframed: 'TBC',
         sizeFramed: 'TBC',
         medium: "TBC",
@@ -70,7 +70,7 @@ const paintings = [
     },
     {
         title: "Wonder",
-        price: null,
+        price: TBC,
         sizeUnframed: 'TBC',
         sizeFramed: 'TBC',
         medium: "TBC",
